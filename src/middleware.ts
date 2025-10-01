2c121f842c691fae416b9da5a498d95c3be32f84
Nur Choirudin <nur.choirudin@mandirisekuritas.co.id>
2025-10-01 15:31:57 +0700

    fixing

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,66 +1,23 @@
-// src/middleware.ts
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-  const { url } = context;
-  const pathname = new URL(url).pathname;
+  const pathname = new URL(context.url).pathname;
 
-  // Handle apple-app-site-association untuk root & .well-known
   if (
     pathname === "/apple-app-site-association" ||
     pathname === "/.well-known/apple-app-site-association"
   ) {
-    const body = {
-      applinks: {
-        apps: [],
-        details: [
-          {
-            "appID": "8YFZ2N3JSC.com.mandirisekuritas.growin.dev",
-            "paths": [
-              "*",
-              "/"
-            ]
-          },
-          {
-            "appID": "8YFZ2N3JSC.com.mandirisekuritas.growin.qa",
-            "paths": [
-              "*",
-              "/"
-            ]
-          },
-          {
-            "appID": "8YFZ2N3JSC.com.mandirisekuritas.growin.staging",
-            "paths": [
-              "*",
-              "/"
-            ]
-          },
-          {
-            "appID": "8YFZ2N3JSC.com.mandirisekuritas.growin",
-            "paths": [
-              "*",
-              "/"
-            ]
-          }
-        ]
-      },
-      webcredentials: {
-        apps: [
-          "8YFZ2N3JSC.com.mandirisekuritas.growin.dev",
-          "8YFZ2N3JSC.com.mandirisekuritas.growin.qa",
-          "8YFZ2N3JSC.com.mandirisekuritas.growin.staging",
-          "8YFZ2N3JSC.com.mandirisekuritas.growin"
-        ]
-      }
-    };
+    const file = await context.locals.runtime.fs.readFile(
+      "public/.well-known/apple-app-site-association",
+      "utf-8"
+    );
 
-    return new Response(JSON.stringify(body), {
+    return new Response(file, {
       headers: {
         "Content-Type": "application/json",
       },
     });
   }
 
-  // Default: lanjut ke handler berikutnya
   return next();
 });
